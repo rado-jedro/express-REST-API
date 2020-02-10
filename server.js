48e403ecfbe75fb2e5c9e80845c3ffc68be8565e
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-10 22:54:32 +0000

    Add db

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 
+const db = [
+    { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+    { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+  ]; 
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
