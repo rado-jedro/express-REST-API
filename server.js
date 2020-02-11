397d8b8e12a859d33c311f1a86e0fce6eaba8fda
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-11 00:22:24 +0000

    Add new endpoints - concerts and seats

--- server.js
+++ server.js
@@ -46,6 +46,73 @@ app.delete('/testimonials/:id', (req, res) => {
   res.json(db.testimonials);
 });
 
+//concerts
+app.get('/concerts', (req, res) => {
+  res.json(db.concerts);
+});
+
+app.get('/concerts/:id', (req, res) => {
+  let idConcert = db.concerts.filter(({ id }) => id == req.params.id);
+  res.json(idConcert);
+});
+
+app.post('/concerts', (req, res) => {
+  let newConcert = {
+    id: uuidv1(),
+    message: 'OK'
+  };
+  db.concerts.push(newConcert);
+  res.json(db.concerts);
+});
+
+app.put('/concerts/:id', (req, res) => {
+  let updateConcert = db.concerts.map(el =>
+    el.id != req.params.id ? el : { id: el.id, message: 'OK' }
+  );
+  db.concerts = updateConcert;
+  res.json(db.concerts);
+});
+
+app.delete('/concerts/:id', (req, res) => {
+  let deletedConcert = db.concerts.filter(el => el.id != req.params.id);
+  db.concerts = deletedConcert;
+  res.json(db.concerts);
+});
+
+
+//seats
+app.get('/seats', (req, res) => {
+  res.json(db.seats);
+});
+
+app.get('/seats/:id', (req, res) => {
+  let idSeat = db.seats.filter(({ id }) => id == req.params.id);
+  res.json(idSeat);
+});
+
+app.post('/seats', (req, res) => {
+  let newSeat = {
+    id: uuidv1(),
+    message: 'OK'
+  };
+  db.seats.push(newSeat);
+  res.json(db.seats);
+});
+
+app.put('/seats/:id', (req, res) => {
+  let updateSeat = db.seats.map(el =>
+    el.id != req.params.id ? el : { id: el.id, message: 'OK' }
+  );
+  db.seats = updateSeat;
+  res.json(db.seats);
+});
+
+app.delete('/seats/:id', (req, res) => {
+  let deletedSeat = db.seats.filter(el => el.id != req.params.id);
+  db.concerts = deletedSeat;
+  res.json(db.seats);
+});
+
 app.use((req, res) => {
   res.status(404).json({
     message: '404 - not found'
