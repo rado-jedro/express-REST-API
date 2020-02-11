b4e27a2faa100965a4f99a89218ab00d63e01270
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-11 21:11:18 +0000

    Add loadSeats to mapDispatchToProps

--- client/src/components/features/OrderTicketForm/OrderTicketFormContainer.js
+++ client/src/components/features/OrderTicketForm/OrderTicketFormContainer.js
@@ -1,13 +1,18 @@
 import { connect } from 'react-redux';
-import { addSeatRequest, getRequests } from '../../../redux/seatsRedux';
+import {
+  addSeatRequest,
+  getRequests,
+  loadSeatsRequest
+} from '../../../redux/seatsRedux';
 import OrderTicketForm from './OrderTicketForm';
 
 const mapStateToProps = state => ({
-  requests: getRequests(state),
+  requests: getRequests(state)
 });
 
 const mapDispatchToProps = dispatch => ({
-  addSeat: (seat) => dispatch(addSeatRequest(seat)),
+  addSeat: seat => dispatch(addSeatRequest(seat)),
+  loadSeats: () => dispatch(loadSeatsRequest())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(OrderTicketForm);
