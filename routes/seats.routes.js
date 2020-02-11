555c65f07a148c89ea3bd7da77f5c2f8e9dc02ba
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-11 01:34:44 +0000

    Create seats.routes.js

---
+++ routes/seats.routes.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./../db');
+const uuidv5 = require('uuid/v5');
+
+// get all seats
+router.route('/seats').get((req, res) => {
+  res.json(db.seats);
+});
+
+// get specific seats
+router.route('/seats/:id').get((req, res) => {
+  let idSeat = db.seats.filter(({ id }) => id == req.params.id);
+  res.json(idSeat);
+});
+
+//add seats
+router.route('/seats').post((req, res) => {
+  const { day, seat, client, email } = req.body;
+  const newSeat = {
+    id: uuidv5(),
+    day: day,
+    seat: seat,
+    client: client,
+    email: email
+  };
+  db.seats.push(newSeat);
+  res.json(db.seats);
+});
+
+//edit seats
+router.route('/seats/:id').put((req, res) => {
+  const { day, seat, client, email } = req.body;
+  let editSeat = db.seats.map(el =>
+    el.id != req.params.id
+      ? el
+      : { id: el.id, day: day, seat: seat, client: client, email: email }
+  );
+  db.seats = editSeat;
+  res.json(db.seats);
+});
+
+//delete seats
+router.route('/seats/:id').delete((req, res) => {
+  let deleteSeat = db.seats.filter(el => el.id != req.params.id);
+  db.seats = deleteSeat;
+  res.json(db.seats);
+});
+
+module.exports = router;
