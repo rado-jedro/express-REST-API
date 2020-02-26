b7ff49c9e1e5163a69066d169dd8ddf3a284da57
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-26 19:07:41 +0000

    Create testimonial controller and testimonial route update

---
+++ controllers/testimonials.controller.js
@@ -0,0 +1,74 @@
+const Testimonial = require('../models/testimonial.model');
+
+exports.getAll = async (req, res) => {
+  try {
+    res.json(await Testimonial.find());
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.getRandom = async (req, res) => {
+  try {
+    const count = await Testimonial.countDocuments();
+    const rand = Math.floor(Math.random() * count);
+    const tes = await Testimonial.findOne().skip(rand);
+    if (!tes) res.status(404).json({ message: 'Not found' });
+    else res.json(tes);
+  } catch (err) {
+    res.json(err);
+  }
+};
+
+exports.getSingleRecord = async (req, res) => {
+  try {
+    const tes = await Testimonial.findById(req.params.id);
+    if (!tes) res.status(404).json({ message: 'Not found' });
+    else res.json(tes);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.postRecord = async (req, res) => {
+  const { id, author, text } = req.body;
+  try {
+    const newTestimonial = new Testimonial({
+      id: id,
+      author: author,
+      text: text
+    });
+    await newTestimonial.save();
+    res.json({ message: 'OK' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.updateRecord = async (req, res) => {
+  const { id, author, text } = req.body;
+  try {
+    const tes = await Testimonial.findById(req.params.id);
+    if (tes) {
+      tes.id = id;
+      tes.author = author;
+      tes.text = text;
+      await tes.save();
+      res.json({ message: 'OK' });
+    } else res.status(404).json({ message: 'Not found...' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.deleteRecord = async (req, res) => {
+  try {
+    const tes = await Testimonial.findById(req.params.id);
+    if (tes) {
+      await Testimonial.deleteOne({ _id: req.params.id });
+      res.json({ message: 'OK' });
+    } else res.status(404).json({ message: 'Not found...' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -1,55 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
-const uuidv1 = require('uuid/v1');
 
-// get all testimonials
-router.route('/testimonials').get((req, res) => {
-  res.json(db.testimonials);
-});
+const TestimonialController = require('../controllers/testimonials.controller');
 
-// get random testimonials
-router.route('/testimonials/random').get((req, res) => {
-  let randomTestimonial =
-    db.testimonials[Math.floor(Math.random() * db.testimonials.length)];
-  res.json(randomTestimonial);
-});
-
-// get specific testimonials
-router.route('/testimonials/:id').get((req, res) => {
-  let idTestimonial = db.testimonials.filter(({ id }) => id == req.params.id);
-  res.json(idTestimonial);
-});
-
-//add testimonials
-router.route('/testimonials').post((req, res) => {
-  const { author, text } = req.body;
-  const newTestimonial = {
-    id: uuidv1(),
-    author: author,
-    text: text
-  };
-  db.testimonials.push(newTestimonial);
-  res.json(db.testimonials);
-});
-
-//edit testimonials
-router.route('/testimonials/:id').put((req, res) => {
-  const { author, text } = req.body;
-  let editTestimonial = db.testimonials.map(el =>
-    el.id != req.params.id ? el : { id: el.id, author: author, text: text }
-  );
-  db.testimonials = editTestimonial;
-  res.json(db.testimonials);
-});
-
-//delete testimonials
-router.route('/testimonials/:id').delete((req, res) => {
-    let deleteTestimonial = db.testimonials.filter(
-    el => el.id != req.params.id
-  );
-  db.testimonials = deleteTestimonial;
-  res.json(db.testimonials);
-});
+router.get('/testimonials', TestimonialController.getAll);
+router.get('/testimonials/random', TestimonialController.getRandom);
+router.get('/testimonials/:id', TestimonialController.getSingleRecord);
+router.post('/testimonials', TestimonialController.postRecord);
+router.put('/testimonials/:id', TestimonialController.updateRecord);
+router.delete('/testimonials/:id', TestimonialController.deleteRecord);
 
 module.exports = router; 
