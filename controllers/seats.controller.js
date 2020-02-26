56021496d39dc8dea64359cbba33231f3dfb7a7d
Rado Jedro <radoslaw.jedro@gmail.com>
2020-02-26 20:11:18 +0000

    Create seat controller and seat route update

---
+++ controllers/seats.controller.js
@@ -0,0 +1,66 @@
+const Seat = require('../models/seat.model');
+
+exports.getAll = async (req, res) => {
+  try {
+    res.json(await Seat.find());
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.getSingleRecord = async (req, res) => {
+  try {
+    const seat = await Seat.findById(req.params.id);
+    if (!seat) res.status(404).json({ message: 'Not found' });
+    else res.json(seat);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.postRecord = async (req, res) => {
+  const { id, day, seat, client, email } = req.body;
+  try {
+    const newSeat = new Seat({
+      id: id,
+      day: day,
+      seat: seat,
+      client: client,
+      email: email
+    });
+    await newSeat.save();
+    res.json({ message: 'OK' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.updateRecord = async (req, res) => {
+  const { id, day, seat, client, email } = req.body;
+  try {
+    const singleSeat = await Seat.findById(req.params.id);
+    if (singleSeat) {
+      singleSeat.id = id;
+      singleSeat.day = day;
+      singleSeat.seat = seat;
+      singleSeat.client = client;
+      singleSeat.email = email;
+      await singleSeat.save();
+      res.json({ message: 'OK' });
+    } else res.status(404).json({ message: 'Not found...' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+exports.deleteRecord = async (req, res) => {
+  try {
+    const seat = await Seat.findById(req.params.id);
+    if (seat) {
+      await Seat.deleteOne({ _id: req.params.id });
+      res.json({ message: 'OK' });
+    } else res.status(404).json({ message: 'Not found...' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -1,61 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
-const uuidv1 = require('uuid/v1');
+const SeatController = require('../controllers/seats.controller');
 
-// get all seats
-router.route('/seats').get((req, res) => {
-  res.json(db.seats);
-});
-
-// get specific seats
-router.route('/seats/:id').get((req, res) => {
-  let idSeat = db.seats.filter(({ id }) => id == req.params.id);
-  res.json(idSeat);
-});
-
-//add seats
-router.route('/seats').post((req, res) => {
-  const { day, seat, client, email } = req.body;
-  const isFree = true;
-
-  for (let booked of db.seats) {
-    if (booked.day === day && booked.seat === seat) {
-      res.json({ message: 'The slot is already taken...' });
-      isFree = false;
-    }
-  }
-  if (isFree === true) {
-    const newSeat = {
-      id: uuidv1(),
-      day: day,
-      seat: seat,
-      client: client,
-      email: email
-    };
-    db.seats.push(newSeat);
-    req.io.emit('seatsUpdated', db.seats);
-    res.json(db.seats);
-  }
-});
-
-//edit seats
-router.route('/seats/:id').put((req, res) => {
-  const { day, seat, client, email } = req.body;
-  let editSeat = db.seats.map(el =>
-    el.id != req.params.id
-      ? el
-      : { id: el.id, day: day, seat: seat, client: client, email: email }
-  );
-  db.seats = editSeat;
-  res.json(db.seats);
-});
-
-//delete seats
-router.route('/seats/:id').delete((req, res) => {
-  let deleteSeat = db.seats.filter(el => el.id != req.params.id);
-  db.seats = deleteSeat;
-  res.json(db.seats);
-});
+router.get('/seats', SeatController.getAll);
+router.get('/seats/:id', SeatController.getSingleRecord);
+router.post('/seats', SeatController.postRecord);
+router.put('/seats/:id', SeatController.updateRecord);
+router.delete('/seats/:id', SeatController.deleteRecord);
 
 module.exports = router;
